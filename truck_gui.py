58685a8f7597631517c98c923b04503dc5fcfb4a
JosephFreze <josephgfreze@gmail.com>
2025-05-08 21:10:04 -0600

    Photo, Status Label, and Size Commit
    I added a photo of a chevy colorado to the folder and imported it into the GUI. I also made it so that there is a status label. Finally, I increased the size of the GUI.
    
    (Much of tkinter needed research to understand)

--- truck_gui.py
+++ truck_gui.py
@@ -11,10 +11,25 @@ from chevy_colorado import Truck
 
 # TODO: Create the main window
 root = Tk()
+root.geometry("1400x800")  # width x height in pixels
 root.title("2025 Chevy Colorado Simulator")
+
+# TODO: Load and display the truck image
+from tkinter import PhotoImage
+
+# Load the image
+truck_img = PhotoImage(file="colorado.png")
+
+# Create a label with the image and pack it
+image_label = Label(root, image=truck_img)
+image_label.pack(pady=10)
 
 # TODO: Create the Truck object
 my_truck = Truck()
+
+# TODO: Label to show truck speed
+status_label = Label(root, text=f"Speed: {my_truck.speed} mph", font=("Arial", 14))
+status_label.pack(pady=10)
 
 # TODO: Run the GUI loop
 root.mainloop()
